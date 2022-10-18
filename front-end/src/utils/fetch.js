a4042baef30b6faf883b08c4092ea3a85b935f22
Bruno Lopes <brunolopesjn@gmail.com>
2022-10-18 15:15:32 -0300

    :bug: fix: Correção de diversos bugs no projeto

--- front-end/src/utils/fetch.js
+++ front-end/src/utils/fetch.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const HOST = process.env.REACT_APP_API_HOST || "localhost";
-const PORT = process.env.REACT_APP_API_PORT || 3001;
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
 
 const fetch = axios.create({
-  baseURL: `http://${HOST}:${PORT}`,
-  timeout: 1000,
+  baseURL: `${PROTOCOL}://${HOST}`,
+  timeout: 10000,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
