f067b0758e0e5c5dee97562f90dc0dd7c4e4de7e
Bruno Lopes <brunolopesjn@gmail.com>
2022-10-18 15:17:48 -0300

    :bug: fix: Correção de diversos bugs no projeto

--- back-end/src/seeders/20221017135436-tasks.js
+++ back-end/src/seeders/20221017135436-tasks.js
@@ -2,12 +2,10 @@ module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('tasks',
       [{
-        id: 1,
         description: 'Estudar os conteúdos de Docker da Trybe',
         check: true
       },
       {
-        id: 2,
         description: 'Resolver o projeto Docker Todo List',
         check: false
       },
